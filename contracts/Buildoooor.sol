e58bf9454aa5165bdc7dc74db7ec97ae86d86d52
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2022-05-15 20:31:16 +0500

    📝

---
+++ contracts/Buildoooor.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Buildoooor is ERC20 {
+
+   
+    uint constant _initial_supply = 50 * (10**18);
+
+    constructor() ERC20("Buildoooor", "BUDL") {
+
+        _mint(msg.sender, _initial_supply);
+    }
+}

373fcae21df39ed8c9687588068a25413ff7902d
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2022-05-15 20:31:56 +0500

    📜

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const weiAmount = (await deployer.getBalance()).toString();
+
+  console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+
+ // make sure to replace the "Buildoooor" reference 
+ //with your own ERC-20 name
+  const Token = await ethers.getContractFactory("Buildoooor");
+  const token = await Token.deploy();
+
+// log the address of the Contract in our console
+  console.log("Token address:", token.address);
+}
+
+
+// run main, catch error, if any, and log in console
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});

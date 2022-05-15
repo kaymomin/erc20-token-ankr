c6fdf5f6daab990dde23050be16aa4dea85c67c9
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2022-05-15 20:33:21 +0500

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
+
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.0",
+  networks:{
+    goerli:{
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
+};
